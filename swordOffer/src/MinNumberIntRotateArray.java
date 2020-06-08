1f22db4d0d2fed66cb89a629aacf500571a3724c
fallenk <liu_lf@zju.edu.cn>
2020-06-08 17:24:21 +0800

    快排 二分等算法

---
+++ swordOffer/src/MinNumberIntRotateArray.java
@@ -0,0 +1,37 @@
+public class MinNumberIntRotateArray {
+
+    // [3, 4, 5, 1, 2]
+    // [1, 2, 3, 4, 5]
+    // 不能使用左边数与中间数比较，这种做法不能有效地减治
+
+    // [1, 2, 3, 4, 5]
+    // [3, 4, 5, 1, 2]
+    // [2, 3, 4, 5 ,1]
+
+    public int minArray(int[] numbers) {
+        int len = numbers.length;
+        if (len == 0) {
+            return 0;
+        }
+        int left = 0;
+        int right = len - 1;
+        while (left < right) {
+            int mid = (left + right) >>> 1;
+            if (numbers[mid] > numbers[right]) {
+                // [3, 4, 5, 1, 2]，mid 以及 mid 的左边一定不是最小数字
+                // 下一轮搜索区间是 [mid + 1, right]
+                left = mid + 1;
+            } else if (numbers[mid] == numbers[right]) {
+                // 只能把 right 排除掉，下一轮搜索区间是 [left, right - 1]
+                right = right - 1;
+            } else {
+                // 此时 numbers[mid] < numbers[right]
+                // mid 的右边一定不是最小数字，mid 有可能是，下一轮搜索区间是 [left, mid]
+                right = mid;
+            }
+        }
+
+        // 最小数字一定在数组中，因此不用后处理
+        return numbers[left];
+    }
+}
--- swordOffer/src/QuickSort.java
+++ swordOffer/src/QuickSort.java
@@ -1,51 +1,50 @@
 public class QuickSort {
     public static void main(String[] args) {
-        int[] arr = {10,7,2,4,7,62,3,4,2,1,8,9,19};
+        int[] arr = new int[]{10,7,2,4,7,62,3,4,2,1,8,100,19};
         QuickSort qs = new QuickSort();
         int[] res = qs.quickSort(arr, 0, arr.length-1);
         for (int i=0; i<arr.length; i++) {
             System.out.println(res[i]);
         }
     }
-
     public int[] quickSort(int[] arr, int start, int end) {
-        if (start == end) return arr;
-        int len = arr.length;
-        int index = partition(arr, start, end); // 位置
-
-        if (index > start) quickSort(arr, start, index-1);
-        if (index < end) quickSort(arr, index+1, end);
-
+       if (start > end) {
+           return null;
+       }
+       if (start == end) {
+           return arr;
+       }
+       int len = arr.length;
+       int index = partition(arr, start, end);
+       if (index > start) quickSort(arr, start, index-1);
+       if (index < end) quickSort(arr, index+1, end);
         return arr;
     }
 
-    public int partition(int[] arr, int left, int right) {
-        if (arr == null || arr.length==0 || left > right) {
+    private int partition(int[] arr, int start, int end) {
+        if (arr == null || start > end || arr.length == 0){
             return -1;
         }
-        int baseVal = arr[left];
+        int baseVal = arr[start];
         int i,j;
-        for (i=left, j=right; i< right && j>=0 && i<j; ) {
-            while (arr[j] > baseVal&& i<j) {
-                j--;
-            }
-            while (arr[i]<=baseVal&& i<j) {
-                i++;
-            }
+        for (i=start, j=end;i<j; ) {
+           while (arr[j] > baseVal && i <j) {
+               j--;
+           }
+           while (arr[i] <= baseVal && i <j) {
+               i++;
+           }
 
-            int temp = 0;
-            if (arr[i] > baseVal && arr[j] <= baseVal) {
+           int temp=0;
+           if (arr[i]>baseVal && arr[j] <=baseVal) {
                 temp = arr[i];
                 arr[i] = arr[j];
                 arr[j] = temp;
-            }
-
+           }
         }
-
-        int temp = arr[left];
-        arr[left] = arr[i];
+        int temp = arr[start];
+        arr[start] = arr[i];
         arr[i] = temp;
         return i;
-
     }
 }
