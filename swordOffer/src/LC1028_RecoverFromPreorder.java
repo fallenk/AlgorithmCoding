572f0111d9b706735c7f58129bb33a1765b3911f
fallenk <liu_lf@zju.edu.cn>
2020-06-29 23:04:04 +0800

    TreeNode

---
+++ swordOffer/src/LC1028_RecoverFromPreorder.java
@@ -0,0 +1,16 @@
+public class LC1028_RecoverFromPreorder {
+    public static void main(String[] args) {
+
+    }
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+
+}
